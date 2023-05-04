676b4429efa9860f686a95b7466b4ce1124032a5
Vladislav Fomenko <vladislav.fomenko.2014@gmail.com>
2023-05-04 13:12:13 +0300

    Almist working automatic tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,8 @@
 #include "ORM.hpp"
 #include "DatabaseMOCK.hpp"
 #include "ORMGenerator.hpp"
-#include <cassert>
 
 int main()
 {
-    ORM::Select("table");
-    
-    assert( == "The request is correct!");
-    
-    assert( == "The request is correct!");
-    ORM::Delete("table", "1");
-    assert( == "The request is correct!");
-    
-    assert( == "The request is correct!");
-   
-    assert( == "The request is correct!");
     return 0;
 }
