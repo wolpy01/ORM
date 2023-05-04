352f36f7e873ee5e063f9675aeb754f79579b482
Vladislav Fomenko <vladislav.fomenko.2014@gmail.com>
2023-05-04 09:49:59 +0300

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,9 @@ int main()
     assert(DatabaseMOCK::execute("SELECT *\nFROM table WHERE predicate;") == "The request is correct!");
     ORM::Find("table", "1");
     assert(DatabaseMOCK::execute("SELECT *\nFROM table WHERE object_id = 1;") == "The request is correct!");
-    ORM::Insert("table", "object");
-    assert(DatabaseMOCK::execute("DELETE *\nFROM table WHERE object_id = 1;") == "The request is correct!");
     ORM::Delete("table", "1");
+    assert(DatabaseMOCK::execute("DELETE *\nFROM table WHERE object_id = 1;") == "The request is correct!");
+    ORM::Insert("table", "object");
     assert(DatabaseMOCK::execute("INSERT INTO table\nVALUES object;") == "The request is correct!");
     ORM::Update("table", "object");
     assert(DatabaseMOCK::execute("UPDATE table\nSET object;") == "The request is correct!");
