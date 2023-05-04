d031808dc9258f1b979d251cbc9f8f1545ed146d
Vladislav Fomenko <vladislav.fomenko.2014@gmail.com>
2023-05-04 13:11:36 +0300

    Almost working automatic tests

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,15 @@
 int main()
 {
     ORM::Select("table");
-    assert(DatabaseMOCK::execute("SELECT *\nFROM table;") == "The request is correct!");
-    ORM::Filter("table", "predicate");
-    assert(DatabaseMOCK::execute("SELECT *\nFROM table WHERE predicate;") == "The request is correct!");
-    ORM::Find("table", "1");
-    assert(DatabaseMOCK::execute("SELECT *\nFROM table WHERE object_id = 1;") == "The request is correct!");
+    
+    assert( == "The request is correct!");
+    
+    assert( == "The request is correct!");
     ORM::Delete("table", "1");
-    assert(DatabaseMOCK::execute("DELETE *\nFROM table WHERE object_id = 1;") == "The request is correct!");
-    ORM::Insert("table", "object");
-    assert(DatabaseMOCK::execute("INSERT INTO table\nVALUES object;") == "The request is correct!");
-    ORM::Update("table", "object");
-    assert(DatabaseMOCK::execute("UPDATE table\nSET object;") == "The request is correct!");
+    assert( == "The request is correct!");
+    
+    assert( == "The request is correct!");
+   
+    assert( == "The request is correct!");
     return 0;
 }
